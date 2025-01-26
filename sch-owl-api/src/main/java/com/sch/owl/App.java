70e39faf694606853f14199672533f5c074037b2
yaodonghu <yaodonghu@rdrk.com.cn>
2025-01-27 00:27:41 +0800

    初步的module架子

---
+++ sch-owl-api/src/main/java/com/sch/owl/App.java
@@ -0,0 +1,13 @@
+package com.sch.owl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
